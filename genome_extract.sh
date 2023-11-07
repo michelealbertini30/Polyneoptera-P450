92b6b0eb6cf7346336ba20e4859eded1a657ee10
michelealbertini30 <michelealbertini30@gmail.com>
2023-11-07 09:50:05 +0100

    Bash Script

--- genome_extract.sh
+++ genome_extract.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 while read line;
-
         do
 
         if echo $line | grep -qv "#";
-
                 then
 
                 ref=$(echo $line | awk '{print $1}')
                 ids=$(echo $line | awk '{print $2}')
 
                 cp $ref/ncbi_dataset/data/$ref/*.fna tmp/$ids
-
         fi
-
 
 done < $1
