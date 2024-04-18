7ca0fdac2f80d51a204d3a8483f56f2c597865ac
michelealbertini30 <michelealbertini30@gmail.com>
2024-04-18 11:34:51 +0200

    Update

--- Busco/busco_single_copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in $(find . -name "full_table.tsv"); do
-	grep -v "^#" ${file} | awk '$2=="Complete" {print $1}' >> complete_busco_ids.txt
-
-done
-
-sort complete_busco_ids.txt | uniq -c | awk '$1 > 36 {print $2}' > final_busco_ids.txt
-
-
